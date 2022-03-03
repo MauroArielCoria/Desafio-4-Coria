170805d50e7c1247be9faf25b1e134e8c9da0fdb
MauroArielCoria <91713335+MauroArielCoria@users.noreply.github.com>
2022-03-03 18:09:59 -0300

    Add files via upload

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,16 @@
+import ItemCount from "./ItemCount"
+
+const ItemListContainer = (greeting) => {
+
+    return (
+    <main>
+        <h2>Bienvenidos!!</h2>
+        <h2>¿Cuántos zapatos va a comprar?</h2>
+
+        <ItemCount/>
+
+    </main>
+    );
+    }
+
+export default ItemListContainer
